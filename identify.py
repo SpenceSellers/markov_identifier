63d1c11a728b88d847731658f7d9da1bf06626b8
Spence <spencesellers@gmail.com>
2014-01-19 13:41:06 -0600

    identify.py written. Program now works, though little testing has been done.

---
+++ identify.py
@@ -0,0 +1,35 @@
+from chain import *
+import sys
+import os
+
+def generate_definitions(directory):
+    files = os.listdir(directory)
+    defs = {}
+    for fname in files:
+        f = open(os.path.join(directory, fname), 'r')
+        c = Chain()
+        c.record_string(f.read())
+        f.close()
+        
+        defs[fname] = c
+    return defs
+
+def main():
+    defs = generate_definitions("references")
+    fname = sys.argv[1]
+    data = None
+    with open(fname) as f:
+        data = f.read()
+
+    results = {}
+    for name, chain in defs.iteritems():
+        results[name] = chain.compare(data)
+    # Sort by the value, not the key.
+    sorted_results = [(v,k) for k, v in results.iteritems()]
+    sorted_results.sort(reverse=True)
+    for value, name in sorted_results:
+        print name + ": " + str(value)
+
+
+if __name__ == "__main__":
+    main()
